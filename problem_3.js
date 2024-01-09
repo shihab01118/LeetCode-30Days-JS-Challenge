33b90c62b43ed04ab09f1cec5054a1c5afb3bded
shihab01118 <shahed.shihab.0@gmail.com>
2024-01-09 19:23:51 +0600

    solved problem 2704: to be or not to be

---
+++ problem_3.js
@@ -0,0 +1,12 @@
+const expect = function(val) {
+    return {
+        toBe: (v) => {
+            if (v === val) return true;
+            else throw new Error("Not Equal");
+        },
+        notToBe: (v) => {
+            if (v !== val) return true;
+            else throw new Error("Equal");
+        }
+    }
+};
